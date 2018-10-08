52728175d409820d482b122650a86ce035d1b254
Ed Tan <edward.tan1@gmail.com>
2018-10-08 16:14:00 -0400

    Fix cache syntax error

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,4 +2,4 @@
 
 JEKYLL_ENV=production jekyll build
 aws s3 sync --exclude "assets/css/styles.css" _site/ s3://techorrect-website
-aws s3 sync --exclude "*" --include "assets/css/styles.css" --metadata-directive REPLACE --content-type "text/css" --cache-control 86400 _site/ s3://techorrect-website
+aws s3 sync --exclude "*" --include "assets/css/styles.css" --metadata-directive REPLACE --content-type "text/css" --cache-control max-age=86400 _site/ s3://techorrect-website
