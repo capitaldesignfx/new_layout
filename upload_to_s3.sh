1b6ed7424003ebfbca63f4e134db8834a44b4a73
Ed Tan <edward.tan1@gmail.com>
2018-10-08 16:11:16 -0400

    Cache styles.css

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 JEKYLL_ENV=production jekyll build
-cd _site
-aws s3 sync . s3://techorrect-website
+aws s3 sync --exclude "assets/css/styles.css" _site/ s3://techorrect-website
+aws s3 sync --exclude "*" --include "assets/css/styles.css" --metadata-directive REPLACE --content-type "text/css" --cache-control 86400 _site/ s3://techorrect-website
