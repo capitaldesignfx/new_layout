ccf423674d8f4eb6c29f51d8ae10359640905446
Ed Tan <edward.tan1@gmail.com>
2018-10-15 16:09:12 -0400

    Fix container locale to pass html-proofer checks

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export LANG=C.UTF-8
+export LANGUAGE=C.UTF-8
+export LC_ALL=C.UTF-8
+
 JEKYLL_ENV=production bundle exec jekyll build
-bundle exec htmlproofer _site/ --http-status-ignore "999" --check-html
+bundle exec htmlproofer _site/ --http-status-ignore "999" --check-html --trace
